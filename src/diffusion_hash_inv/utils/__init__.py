cf531c9ac2e2da7fe513bbe9ba956a059c3e7db6
andy0000-droid <andy020207@gmail.com>
2025-09-03 13:54:42 +0900

    update __init.py

--- src/diffusion_hash_inv/utils/__init__.py
+++ src/diffusion_hash_inv/utils/__init__.py
@@ -5,13 +5,15 @@
 
 # 코드베이스에 따라 클래스명이 FileIO 또는 FILEio 일 수 있어 호환 처리
 try:
-    from .file_io import FileIO  # 권장 표기
     from .project_root import add_root_to_path, add_src_to_path  # 프로젝트 루트 경로 설정 유틸
+    from .file_io import FileIO  # 권장 표기
+    from .json_formatter import OutputFormat
 except ImportError:  # 기존 코드 호환
     from .file_io import FILEio as FileIO
 
 __all__ = [
     "FileIO",
     "add_root_to_path",
-    "add_src_to_path"
+    "add_src_to_path",
+    "OutputFormat"
 ]
